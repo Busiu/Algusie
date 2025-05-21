7c7d53372c3593cf39f8d09a67f1d8deba5f9206
Wojciech B. <busiu69@gmail.com>
2025-05-21 21:54:11 +0200

    Add sieve

---
+++ src/math/SieveOfEratosthenes.java
@@ -0,0 +1,25 @@
+package math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SieveOfEratosthenes {
+    private List<Integer> sieve(int n) {
+        var notPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i]) {
+                continue;
+            }
+            for (int j = i * i; j < n; j += i) {
+                notPrime[j] = true;
+            }
+        }
+        var result = new ArrayList<Integer>();
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
