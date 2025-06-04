edefaa0dc15c95b09515f3d6304e006a6405d9ac
Wojciech B. <busiu69@gmail.com>
2025-06-04 22:49:59 +0200

    Add FenwickTree

---
+++ src/datastructures/FenwickTree.java
@@ -0,0 +1,32 @@
+package datastructures;
+
+class FenwickTree {
+    int n;
+    int[] fenTree;
+
+    FenwickTree(int[] arr) {
+        this.n = arr.length + 1;
+        this.fenTree = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            add(i, arr[i]);
+        }
+    }
+
+    void add(int i, int val) {
+        i++;
+        while (i < n) {
+            fenTree[i] += val;
+            i += (i & -i);
+        }
+    }
+
+    int sum(int i) {
+        i++;
+        var sum = 0;
+        while (i > 0) {
+            sum += fenTree[i];
+            i -= (i & -i);
+        }
+        return sum;
+    }
+}
