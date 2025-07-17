05610766fc16bfafcfc604d89d6578777bf5e6ad
Wojciech B. <busiu69@gmail.com>
2025-07-18 01:08:34 +0200

    Add Binary Lifting with LCA

--- src/tree/BinaryLifting.java
+++ src/tree/BinaryLifting.java
@@ -1,39 +1,70 @@
 package tree;
 
-// TODO: Implementation given int[] children -> (DFS)
-// TODO: Implementation for 3585 (depth (no need for anc == -1 check), dist from root, dist from ancestor itp...)
-//       For this, use preprocessing -> query logic.
+import java.util.ArrayList;
 
-public class BinaryLifting {
-    int log;
+class BinaryLifting {
+    int maxPower;
+    int[] depth;
+    long[] dist;
     int[][] up;
+    ArrayList<ArrayList<int[]>> cons;
 
-    // Assuming parents[0] = -1;
-    // Otherwise -> add sentinel node : parents[root] = n + 1. If any query returns n + 1 -> we have exceeded the top.
-    BinaryLifting(int n, int[] parents) {
-        log = 0;
-        while (1 << log <= n) log++;
+    BinaryLifting(int n, ArrayList<ArrayList<int[]>> cons) {
+        this.maxPower = 0;
+        while (1 << maxPower <= n) this.maxPower++;
 
-        this.up = new int[n][log];
-        for (int i = 0; i < n; i++) {
-            up[i][0] = parents[i];
+        this.depth = new int[n];
+        this.dist = new long[n];
+        this.up = new int[n][maxPower];
+        this.cons = cons;
+
+        depth[0] = 0;
+        dist[0] = 0;
+        for (int i = 0; i < maxPower; i++) {
+            up[0][i] = 0;
         }
-        for (int lvl = 1; lvl < log; lvl++) {
-            for (int i = 0; i < n; i++) {
-                int anc = up[i][lvl - 1];
-                if (anc == -1) up[i][lvl] = -1;
-                else up[i][lvl] = up[up[i][lvl - 1]][lvl - 1];
-            }
+
+        for (int[] child : cons.get(0)) {
+            dfs(child[0], 0, child[1]);
         }
     }
 
-    int getKthAncestor(int node, int k) {
-        int anc = node;
-        for (int i = 0; i < log; i++) {
-            if ((k & (1 << i)) > 0) {
-                anc = up[anc][i];
+    void dfs(int cur, int prev, int edgeLen) {
+        depth[cur] = depth[prev] + 1;
+        dist[cur] = dist[prev] + (long) edgeLen;
+        up[cur][0] = prev;
+        for (int i = 1; i < maxPower; i++) {
+            up[cur][i] = up[up[cur][i - 1]][i - 1];
+        }
+
+        for (int[] child : cons.get(cur)) {
+            if (child[0] == prev) continue;
+            dfs(child[0], cur, child[1]);
+        }
+    }
+
+    int lca(int node1, int node2) {
+        if (depth[node1] < depth[node2]) {
+            int tmp = node1;
+            node1 = node2;
+            node2 = tmp;
+        }
+        int diff = depth[node1] - depth[node2];
+        for (int i = 0; i < maxPower; i++) {
+            if ((diff & (1 << i)) > 0) {
+                node1 = up[node1][i];
             }
         }
-        return anc;
+
+        if (node1 == node2) return node1;
+
+        for (int i = maxPower - 1; i >= 0; i--) {
+            if (up[node1][i] != up[node2][i]) {
+                node1 = up[node1][i];
+                node2 = up[node2][i];
+            }
+        }
+
+        return up[node1][0];
     }
 }
---
+++ src/tree/BinaryLiftingSimplified.java
@@ -0,0 +1,35 @@
+package tree;
+
+public class BinaryLiftingSimplified {
+    int log;
+    int[][] up;
+
+    // Assuming parents[0] = -1;
+    // Otherwise -> add sentinel node : parents[root] = n + 1. If any query returns n + 1 -> we have exceeded the top.
+    BinaryLiftingSimplified(int n, int[] parents) {
+        log = 0;
+        while (1 << log <= n) log++;
+
+        this.up = new int[n][log];
+        for (int i = 0; i < n; i++) {
+            up[i][0] = parents[i];
+        }
+        for (int lvl = 1; lvl < log; lvl++) {
+            for (int i = 0; i < n; i++) {
+                int anc = up[i][lvl - 1];
+                if (anc == -1) up[i][lvl] = -1;
+                else up[i][lvl] = up[up[i][lvl - 1]][lvl - 1];
+            }
+        }
+    }
+
+    int getKthAncestor(int node, int k) {
+        int anc = node;
+        for (int i = 0; i < log; i++) {
+            if ((k & (1 << i)) > 0) {
+                anc = up[anc][i];
+            }
+        }
+        return anc;
+    }
+}
