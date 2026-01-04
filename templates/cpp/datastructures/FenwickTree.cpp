4d332102df064548b95d8c034ed118fccac298a6
Wojciech B. <busiu69@gmail.com>
2026-01-04 01:58:29 +0100

    Add FenwickTree (cpp)

---
+++ templates/cpp/datastructures/FenwickTree.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct FenwickTree {
+    vector<int> tree;
+    
+    FenwickTree(int size) {
+        tree.assign(size + 1, 0);
+    }
+
+    void add(int idx, int val) {
+        idx++;
+        while (idx < tree.size()) {
+            tree[idx] += val;
+            idx += (idx & -idx);
+        }
+    }
+
+    int sum(int idx) {
+        idx++;
+        int ans = 0;
+        while (idx > 0) {
+            ans += tree[idx];
+            idx -= (idx & -idx);
+        }
+        return ans;
+    }
+}
